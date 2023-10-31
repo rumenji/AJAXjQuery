47ceace42b2f92cbf184b79fee585b33508294b8
rumenji <rumen_ji@yahoo.com>
2023-10-31 14:26:35 -0400

    Completed Ajax and jQuery Exercise

---
+++ js/nav.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+$navLogin.on("click", navLoginClick);
+
+//Show submit new story form
+function submitClick(evt) {
+  hidePageComponents();
+  $loginForm.hide();
+  $signupForm.hide();
+  $storyForm.show();
+}
+$navSubmit.on("click", submitClick);
+
+//Show favorite stories only
+function favoritesClick(evt) {
+  putFavoritesOnPage();
+}
+$navFavorites.on("click", favoritesClick);
+
+//Show user stories only
+function myStoriesClick(evt) {
+  putOwnStoriesOnPage();
+}
+$navMyStories.on("click", myStoriesClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+
---
+++ js/stories.js
@@ -0,0 +1,118 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+// Add new story after story form is submit
+function submitStoryForm(evt){
+    evt.preventDefault();
+  
+    // grab the form values
+    const title = $("#title").val();
+    const author = $("#author").val();
+    const url = $("#url").val();
+  
+    $storyForm.trigger("reset");
+    $storyForm.hide();
+
+  
+    storyList.addStory(currentUser, {title, author, url});
+    // getAndShowStoriesOnStart();
+    start();
+
+  }
+  
+$storyForm.on("submit", submitStoryForm);
+
+//favorite story
+function favoriteStory(evt) {
+  evt.preventDefault();
+  currentUser.storeFavorites(evt.target.parentElement.parentElement.id)
+}
+$allStoriesList.on("click", ".favorite", favoriteStory);
+
+function deleteStory(evt) {
+  evt.preventDefault();
+  currentUser.removeStory(evt.target.parentElement.parentElement.id)
+}
+
+$allStoriesList.on("click", ".delete", deleteStory);
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  let deleteIcon = '';
+  let favoriteIcon = '';
+  const hostName = story.getHostName();
+  if (currentUser){
+    if(currentUser.ownStories.find(s => s.storyId === story.storyId)){
+      console.log(story)
+      deleteIcon = '<span class="delete"><i class="fas fa-trash-alt"></i></span>';
+    } 
+    if (currentUser.favorites.find(s => s.storyId === story.storyId)){
+      console.log(story)
+      favoriteIcon = '<span class="favorite"><i class="fas fa-star"></i></span>'
+    } else if (!currentUser.favorites.find(s => s.storyId === story.storyId)){
+      favoriteIcon = '<span class="favorite"><i class="far fa-star"></i></span>'
+    }
+  }
+    
+  return `
+  <li id="${story.storyId}">
+    ${deleteIcon}
+    ${favoriteIcon}
+    <a href="${story.url}" target="a_blank" class="story-link">
+      ${story.title}
+    </a>
+    <small class="story-hostname">(${hostName})</small><br>
+    <small class="story-author">by ${story.author}</small><br>
+    <small class="story-user">posted by ${story.username}</small>
+  </li>
+  <hr>
+`;
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+function putOwnStoriesOnPage() {
+  $allStoriesList.empty();
+  for(let story of currentUser.ownStories){
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  } 
+}
+
+function putFavoritesOnPage() {
+  $allStoriesList.empty();
+  for(let story of currentUser.favorites){
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  } 
+}
